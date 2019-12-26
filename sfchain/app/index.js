eae2c024d63f0a83e576fd3b2a1f49a24c766cbd
snag <somjit.nag@in.imshealth.com>
2019-12-27 01:45:51 +0530

    ch08: added http get & post transaction endpoints

--- sfchain/app/index.js
+++ sfchain/app/index.js
@@ -3,16 +3,32 @@ const bodyparser = require("body-parser");
 const Blockchain = require("../blockchain");
 const P2PServer = require("./p2p-server");
 
+const Wallet = require("../wallet");
+const TransactionPool = require("../wallet/TransactionPool");
+
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2PServer(bc);
 
 app.use(bodyparser.json());
 
 app.get("/blocks", (req, res) => {
   res.json(bc.chain);
+});
+
+app.get("/transactions", (req, res) => {
+  res.json(tp.transactions);
+});
+
+app.post("/transact", (req, res) => {
+  const { recipient, amount } = req.body;
+  const tx = wallet.createTransaction(recipient, amount, tp);
+  // ^^ transaction already updated to pool, so we should now be able to see it
+  res.redirect("/transactions");
 });
 
 app.post("/mine", (req, res) => {
